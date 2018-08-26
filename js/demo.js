ea9315a02a941750264f21d15941555921ccfdc5
Sabuj <sabujlovescoding@gmail.com>
2018-08-26 13:15:15 +0530

    finally a typeit.js

--- js/demo.js
+++ js/demo.js
@@ -29,6 +29,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // import TypeIt from 'typeit';
 
+
+  var typed2 = new Typed('.typeit', {
+    strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>', 'Chars &times; &copy;'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true
+  });
+
  
 
 
