6a90356013a94097d0c2f31026e522af7693ab37
Sabuj <sabujlovescoding@gmail.com>
2018-08-26 09:42:45 +0530

    test 1

---
+++ demo/js/demo.js
@@ -0,0 +1,35 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#fbc531',
+    lineColor: '#dcdde1',
+    density: '10000',
+    parallax: false,
+    lineWidth: 0.2,
+    directionX: 'right',
+    directionY: 'up',
+    particleRadius: 4
+    
+
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
